fdb5e325135af91d791e18f426a41388f35987a9
Adam Tretera <adam.tretera@ackee.cz>
2023-04-28 09:52:39 +0200

    :sparkles: Blog components

---
+++ src/modules/blog/components/BlogList.tsx
@@ -0,0 +1,12 @@
+import { Blog } from "./Blog";
+
+export function BlogList() {
+  return (
+    <>
+      <h1>BlogList</h1>
+      <Blog title={"iPhone 14 "} content={"lreoamarre remremkranre"} />
+      <Blog title={" iPhone 16 pro max"} content={"lreoamarre remremkranre"} />
+      <Blog title={"AMD ryzen 7950"} content={"lreoamarre remremkranre"} />
+    </>
+  );
+}
